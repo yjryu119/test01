6eb5eeece50671eab812e02049970a7a9ac4ba1f
April Yeojeong Ryu <april@Aprilui-MacBookPro.local>
2022-06-10 02:24:05 +0900

    homepage error fix

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <BrowserRouter basename="/test01/">
+      <BrowserRouter basename="/test01">
         <ScrollTop />
         <NavBar keyword={keyword} setKeyword={setKeyword}></NavBar>
         <Routes>
